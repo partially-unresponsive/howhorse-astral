534951d4618cc50ce34a953f7eacf0a33fa3f59e
CutieSai <unsecuredmail@proton.me>
2025-06-20 00:08:03 +0300

    fix: Folder structure

---
+++ src/main.js
@@ -0,0 +1,5 @@
+console.log("mrrow purr :3");
+function greetchu(person, date) {
+    console.log("mrrow mrrow ".concat(person, " nya, today is ").concat(date.toLocaleDateString(), "!"));
+}
+greetchu("Maddy Thorson", new Date());
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+console.log("mrrow purr :3");
+function greetchu(person: string, date: Date) {
+    console.log(`mrrow mrrow ${person} nya, today is ${date.toLocaleDateString()}!`);  
+}
+
+greetchu("Maddy Thorson", new Date());
